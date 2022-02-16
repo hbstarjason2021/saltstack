d2725a5b78368bb7325b6e736eb8f6350604007a
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-02-17 00:44:38 +0800

    Update master.sh

--- master.sh
+++ master.sh
@@ -12,6 +12,9 @@ echo "deb [signed-by=/usr/share/keyrings/salt-archive-keyring.gpg arch=amd64] ht
 sudo apt-get update
 
 sudo apt-get install salt-master salt-minion -y
+
+cp /etc/salt/master{,.back}
+
 sed -i "s/#auto_accept: False/auto_accept: True/g" /etc/salt/master
 sed -i "s/#master: salt/master: 192.168.56.20/g" /etc/salt/minion
 
