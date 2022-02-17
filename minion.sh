20dee75e43958df801c4b07a9695984b95693851
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-02-18 02:06:24 +0800

    Update minion.sh

--- minion.sh
+++ minion.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+cp /etc/apt/sources.list /etc/apt/sources.list.bak
+sed -i "s/archive.ubuntu.com/mirrors.aliyun.com/g" /etc/apt/sources.list
+sed -i '/^#/d' /etc/apt/sources.list
+
 # Download key
 sudo curl -fsSL -o /usr/share/keyrings/salt-archive-keyring.gpg https://repo.saltproject.io/py3/ubuntu/20.04/amd64/latest/salt-archive-keyring.gpg
 # Create apt sources list file
