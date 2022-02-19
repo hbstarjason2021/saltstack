bf6483257f41da8717925cebaeadd6872f3a19e4
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-02-20 02:20:05 +0800

    Update minion.sh

--- minion.sh
+++ minion.sh
@@ -12,11 +12,19 @@ sudo curl -fsSL -o /usr/share/keyrings/salt-archive-keyring.gpg https://repo.sal
 echo "deb [signed-by=/usr/share/keyrings/salt-archive-keyring.gpg arch=amd64] https://repo.saltproject.io/py3/ubuntu/20.04/amd64/latest focal main" | sudo tee /etc/apt/sources.list.d/salt.list
 
 sudo apt-get update
-
 sudo apt-get install salt-minion -y
 
 cp /etc/salt/minion{,.back}
 
 sed -i "s/#master: salt/master: 192.168.56.20/g" /etc/salt/minion
+
+cat >>  /etc/salt/minion <<EOF
+saltenv: int
+EOF
+
+cat >> /etc/salt/grains <<EOF
+roles:
+  - prometheus
+EOF
 
 systemctl restart salt-minion
